1d82ed263ff387ce19ac7c4287c9ffb97ba37fc2
Berto Mill <bertmill19@gmail.com>
2024-11-18 19:56:04 -0500

    fix: Convert next.config.js to CommonJS syntax
    - Remove ESM import/export
    - Add JSDoc type annotation
    - Add Medium image domains to config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['cdn-images-1.medium.com', 'miro.medium.com'],
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
